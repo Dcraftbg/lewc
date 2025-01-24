79913053fbfb159221a092cce0d350a137e7e669
DcraftBg <czl_obshta@abv.bg>
2025-01-24 13:03:25 +0200

    Simple program for generating tests

---
+++ examples/gentest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Please specify name"
+    echo "Usage: $0 <path without .lew>"
+    exit 1
+fi
+
+test_list="./bin/lewc examples/$1.lew -o ./int/tests/$1.s
+as ./int/tests/$1.s -o ./int/tests/$1.o
+gcc -static ./int/tests/$1.o -o ./bin/tests/$1
+./bin/tests/$1"
+echo "$test_list" > "$1.lew.test.list"
