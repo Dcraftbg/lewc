2df93597c4b60901be53c2f8399ce56a943d00f5
DcraftBg <czl_obshta@abv.bg>
2025-01-26 12:53:46 +0200

    [testsys] Add string_parse example to test list

--- testsys/src/main.c
+++ testsys/src/main.c
@@ -14,6 +14,7 @@ const char* all_tests[] = {
     "nested.lew",
     "scope.lew",
     "suffixes.lew",
+    "string_parse.lew",
 };
 #define python_interp(cmd) nob_cmd_append(cmd, "python3")
 #define python_rere(cmd) (python_interp(cmd), nob_cmd_append(&cmd, 
