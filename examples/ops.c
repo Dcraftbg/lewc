04caa2529d60d0af90616c29dd7977fcbee035cb
DcraftBg <czl_obshta@abv.bg>
2025-02-02 11:40:11 +0200

    [examples] ops test case

---
+++ examples/ops.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define BINOPS \
+    X(add, +) \
+    X(sub, -) \
+    X(mul, *) \
+    X(div, /) \
+    X(and, &) \
+    X(or , |) \
+    X(xor, ^)
+
+#define X(name, ...) \
+    int name##_lew (int a, int b);
+BINOPS
+#undef X
+#define STRINGIFY(x) # x
+int main(void) {
+    size_t n = 0, failed = 0;
+    int a = 4, b = 5;
+    int expected, actual;
+#define X(name, op) \
+    fprintf(stderr, "[Test %zu] %d %s %d => %d (%s)...", n++, a, STRINGIFY(op), b, name##_lew (a, b), STRINGIFY(name##_lew)); \
+    expected = a op b; \
+    actual = name##_lew (a, b); \
+    if(expected == actual) fprintf(stderr, "OK\n"); \
+    else { \
+        failed++; \
+        fprintf(stderr, "Expected %d got %d\n", expected, actual); \
+    }
+    BINOPS
+    if(failed) {
+        fprintf(stderr, "Failed %zu/%zu\n", failed, n);
+        return 1;
+    }
+    return 0;
+}
---
+++ examples/ops.lew.test.list.bi
@@ -0,0 +1,36 @@
+:i count 4
+:b shell 48
+./bin/lewc examples/ops.lew -o ./int/tests/ops.s
+:i returncode 0
+:b stdout 0
+
+:b stderr 0
+
+:b shell 41
+as ./int/tests/ops.s -o ./int/tests/ops.o
+:i returncode 0
+:b stdout 0
+
+:b stderr 0
+
+:b shell 65
+gcc -static ./int/tests/ops.o ./examples/ops.c -o ./bin/tests/ops
+:i returncode 0
+:b stdout 0
+
+:b stderr 0
+
+:b shell 15
+./bin/tests/ops
+:i returncode 0
+:b stdout 0
+
+:b stderr 246
+[Test 0] 4 + 5 => 9 (add_lew)...OK
+[Test 1] 4 - 5 => -1 (sub_lew)...OK
+[Test 2] 4 * 5 => 20 (mul_lew)...OK
+[Test 3] 4 / 5 => 0 (div_lew)...OK
+[Test 4] 4 & 5 => 4 (and_lew)...OK
+[Test 5] 4 | 5 => 5 (or_lew)...OK
+[Test 6] 4 ^ 5 => 1 (xor_lew)...OK
+
