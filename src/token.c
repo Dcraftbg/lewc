ed696a9c15c992c0ab2e6e6d0577342ce06bfb2a
DcraftBg <czl_obshta@abv.bg>
2025-02-04 22:13:25 +0200

    [token] Remove static assert

--- src/token.c
+++ src/token.c
@@ -3,7 +3,6 @@
 #define TPRINTF(...) snprintf(tdisplay_buf, sizeof(tdisplay_buf), __VA_ARGS__)
 const char* tdisplay(Token t) {
     static char tdisplay_buf[1024];
-    static_assert(TOKEN_COUNT == 275, "Update tdisplay");
     assert(sizeof(tdisplay_buf) > (t.src_len + 1));
     strncpy(tdisplay_buf, t.src, t.src_len);
     return tdisplay_buf;
