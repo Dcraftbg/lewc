987f5ddefd64cc1ca5301012429bc7d7feaeb050
DcraftBg <czl_obshta@abv.bg>
2025-02-26 19:07:06 +0200

    [token] Add tdisplay for errors

--- src/token.c
+++ src/token.c
@@ -2,10 +2,31 @@
 #include "strutils.h"
 #define TPRINTF(...) snprintf(tdisplay_buf, sizeof(tdisplay_buf), __VA_ARGS__)
 const char* tdisplay(Token t) {
+    static_assert(TOKEN_COUNT-TOKEN_ERR == 3, "Update tdisplay errors");
     static char tdisplay_buf[1024];
-    assert(sizeof(tdisplay_buf) > (t.src_len + 1));
-    strncpy(tdisplay_buf, t.src, t.src_len);
-    return tdisplay_buf;
+    switch(t.kind) {
+    case TOKEN_EOF:
+        TPRINTF("EOF");
+        return tdisplay_buf;
+    case TOKEN_UNPARSABLE:
+        if(t.codepoint <= 127) {
+           TPRINTF("UNPARSABLE (%c)",(char)t.codepoint);
+        } else {
+           TPRINTF("UNPARSABLE (%08X)",t.codepoint);
+        }
+        return tdisplay_buf;
+    case TOKEN_INVALID_STR:
+        TPRINTF("Invalid String");
+        return tdisplay_buf;
+    case TOKEN_INVALID_INT_LITERAL:
+        TPRINTF("Invalid integer literal");
+        return tdisplay_buf;
+    default:
+        assert(sizeof(tdisplay_buf) > (t.src_len + 1));
+        strncpy(tdisplay_buf, t.src, t.src_len);
+        return tdisplay_buf;
+    }
+    unreachable("Non exhaustive tdisplay");
 }
 
 const char* tloc(Token t) {
